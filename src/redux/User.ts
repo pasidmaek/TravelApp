7119809b2a796065b098507e9285109b46562621
Pasid Khumjanad <pasid.khu@itmx.co.th>
2023-07-31 20:00:40 +0700

    Initial commit

--- src/redux/User.ts
+++ src/redux/User.ts
@@ -1,12 +1,4 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const User = () => {
-  return (
-    <View>
-      <Text>User</Text>
-    </View>
-  );
-};
-
 export default User;
